972ce784e4af0fd0966d4a86aeb353cc6c9ff16e
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-04-18 15:00:42 +0630

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static('public'));
 
 // --- MONGODB CHAT SETUP ---
 mongoose.connect(process.env.MONGO_URI)
